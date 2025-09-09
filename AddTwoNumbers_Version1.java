64473d08e4e0f17de4c055155c94e0169bf5d589
vishleshaa <vishlesharao@gmail.com>
2025-09-09 14:48:23 +0530

    Add files via upload

---
+++ AddTwoNumbers_Version1.java
@@ -0,0 +1,8 @@
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int sum = a + b;
+        System.out.println("The sum is: " + sum);
+    }
+}
